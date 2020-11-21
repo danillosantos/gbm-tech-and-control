dddf15cc1a6dc53ad73605714d6d81064a05d2f9
Luiz Reis <luiz@devmagic.com.br>
2020-11-21 12:32:03 -0300

    chore(*): dashboard was updated with all the details and business rules of the application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule, LOCALE_ID } from "@angular/core";
+import { registerLocaleData } from "@angular/common";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import localePt from "@angular/common/locales/pt";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NbThemeModule, NbLayoutModule, NbContextMenuModule, NbActionsModule, NbMenuModule } from '@nebular/theme';
+registerLocaleData(localePt);
+import {
+  NbThemeModule,
+  NbLayoutModule,
+  NbContextMenuModule,
+  NbActionsModule,
+  NbMenuModule
+} from "@nebular/theme";
+import { DashboardService } from "./dashboard.service";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    NbThemeModule.forRoot({ name: 'default' }),
+    NbThemeModule.forRoot({ name: "default" }),
     NbLayoutModule,
     NbActionsModule,
     NbMenuModule.forRoot(),
     NbContextMenuModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [{ provide: LOCALE_ID, useValue: "pt-BR" }, DashboardService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,20 +1,31 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { NbButtonModule, NbCardModule, NbDialogModule } from '@nebular/theme';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import {
+  NbButtonModule,
+  NbCardModule,
+  NbDialogModule,
+  NbSpinnerModule
+} from "@nebular/theme";
 
-import { HomeRoutingModule } from './home-routing.module';
-import { HomeComponent } from './home.component';
+import { HomeRoutingModule } from "./home-routing.module";
+import { HomeComponent } from "./home.component";
+import { DashboardService } from "../dashboard.service";
+import { HttpClient, HttpClientModule } from "@angular/common/http";
 
 @NgModule({
-  declarations: [
-    HomeComponent,
-  ],
+  declarations: [HomeComponent],
   imports: [
     CommonModule,
     NbCardModule,
     NbButtonModule,
     NbDialogModule.forRoot(),
     HomeRoutingModule,
+    HttpClientModule,
+    NbSpinnerModule
+  ],
+  providers: [
+    // { provide: LOCALE_ID, useValue: "pt-BR" },
+    DashboardService
   ]
 })
-export class HomeModule { }
+export class HomeModule {}
